e40dc0dba900eed44d59e3fed4b9c75337bd2251
ziyenl <ziyenlerm@googlemail.com>
2016-09-26 13:22:33 +0800

    Update settings.py

--- siteproj/settings.py
+++ siteproj/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'siteproj',                      # Or path to database file if using sqlite3.
-        'USER': 'ziyenl',                      # Not used with sqlite3.
-        'PASSWORD': 'ziyenl',                  # Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
